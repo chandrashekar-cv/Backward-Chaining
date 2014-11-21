29a4a583e4f784022cd9decdace8651914395c79
Chandrashekar Venugopala Chimbili <chandrashekar.cv@outlook.com>
2014-11-21 02:48:40 -0800

    version 2.

--- AI_HW3/src/com/BackwardChaining.java
+++ AI_HW3/src/com/BackwardChaining.java
@@ -156,42 +156,6 @@ public class BackwardChaining {
 		
 	}
 	
-	
-	Query parseQuery(String queryString)
-	{	
-		String[] data = queryString.split("&|=>");
-		Query input = createPremise(data[data.length-1]);
-		
-		for (int i =0;i<data.length-1;i++) {
-			System.out.println(data[i]);
-			input.premises.add(createPremise(data[i]));
-		}
-	
-		return input;
-	}
-	
-	Query createPremise(String premise)
-	{
-		Query pObj = new Query();
-		String name = premise.substring(0, premise.indexOf("("));
-		pObj.name = name;
-		
-		String[] variables= premise.substring(premise.indexOf("(")+1,premise.lastIndexOf(")")).split(",");
-		
-		for (String var : variables) {
-			String value=null;
-			
-			if(((int)var.charAt(0))>96)
-				value="";
-			else 
-				value=var;
-			
-			pObj.variable.put(var,value);
-		}
-		
-		return pObj;
-	}
-	
 	void insertIntoKB(Query input)
 	{
 		if(KB.containsKey(input.name))
@@ -241,4 +205,39 @@ public class BackwardChaining {
 		return true;
 	}
 	
+	
+	Query parseQuery(String queryString)
+	{	
+		String[] data = queryString.split("&|=>");
+		Query input = createPremise(data[data.length-1]);
+		
+		for (int i =0;i<data.length-1;i++) {
+			
+			input.premises.add(createPremise(data[i]));
+		}
+	
+		return input;
+	}
+	
+	Query createPremise(String premise)
+	{
+		Query pObj = new Query();
+		String name = premise.substring(0, premise.indexOf("("));
+		pObj.name = name;
+		
+		String[] variables= premise.substring(premise.indexOf("(")+1,premise.lastIndexOf(")")).split(",");
+		
+		for (String var : variables) {
+			String value=null;
+			
+			if(((int)var.charAt(0))>96)
+				value="";
+			else 
+				value=var;
+			
+			pObj.variable.put(var,value);
+		}
+		
+		return pObj;
+	}
 }
